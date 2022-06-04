f6486bb4a19170a33b328f087d619daf49dfbd27
Nahid Habibov <nahid.habibov@accessbank.az>
2022-06-04 15:47:19 +0400

    add feature

--- GitProj/UI/Main/Controller/ViewController.swift
+++ GitProj/UI/Main/Controller/ViewController.swift
@@ -12,8 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        showComoponents()
     }
 
+    func showComoponents() {
+        print("Hello")
+    }
 
 }
 
